be90fcf8ba5a5098f8f80dec81ec3cf486dac00c
wh1pch81n <wh1pch81n@gmail.com>
2014-03-02 16:58:46 -0800

    Added a find function

--- Linked_List/main.cpp
+++ Linked_List/main.cpp
@@ -11,6 +11,12 @@
 
 using namespace std;
 
+/**
+ inserts a node to the beginging of the list
+ @param head a reference to the pointer of the head of the list
+ @param data the data value that you want to store
+ @return if the insertion was a success then it will return true
+ */
 template <class T>
 bool prependList (Node<T> *&head, T data) {
 	Node<T> *newInstance = new Node<T>(data);
@@ -22,12 +28,33 @@ bool prependList (Node<T> *&head, T data) {
 	return true;
 }
 
+/**
+ traverses the list
+ @param node a node that will be the starting point of the traversal all the way to the end
+ @param action a block pointer that will be called on the currect node.
+ */
 template <class z>
 void traverseList(Node<z> *node, void(^action)(Node<z> *node)) {
   if (node) {
 		action(node);
 		traverseList(node->getNext(), action);
 	}
+}
+
+/**
+ finds the node that contains the given value
+ @param
+ */
+template <class T>
+Node<T> *find(Node<T> *node, T data) {
+  __block Node<T> *cur = NULL;
+	traverseList(node, ^(Node<T> *node){
+		if (node->getItem() == data) {
+			cur = node;
+		}
+	});
+
+	return cur;
 }
 
 int main(int argc, const char * argv[])
@@ -45,5 +72,13 @@ int main(int argc, const char * argv[])
 	traverseList(head, ^(Node<int> *node){
 		cout << node->getItem() << "  " << node->getNext() << endl;
 	});
+	
+	cout << find(head, 9) << endl;
+	cout << find(head, 4) << endl;
+	cout << find(head, 3) << endl;
+	cout << find(head, 0) << endl;
+	
+	
+	
 	return 0;
 }
