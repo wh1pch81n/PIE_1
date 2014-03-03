e91f0625625af0cc7a4de627e5d651083b8d85f8
wh1pch81n <wh1pch81n@gmail.com>
2014-03-02 16:34:20 -0800

    Initial Commit

---
+++ Linked_List/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Linked_List
+//
+//  Created by Derrick Ho on 3/2/14.
+//  Copyright (c) 2014 Derrick Ho. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
